0e9b6b20008b6c170473c6a49715f57f29c570cf
Yogesh Singh <yogeshsinghbhati03@gmail.com>
2021-06-09 23:46:19 +0530

    Daily Practice

--- Functions/MaxAndMin.cpp
+++ Functions/MaxAndMin.cpp
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-int Max(int n1,int n2,int n3){
-    if(n1>n2){
-        if(n1>n3)
+
+int Max(int n1, int n2, int n3)
+{
+    if (n1 > n2)
+    {
+        if (n1 > n3)
             return n1;
         else
             return n3;
@@ -14,9 +16,11 @@ int Max(int n1,int n2,int n3){
         return n3;
 }
 
-int Min(int n1,int n2,int n3){
-    if(n1<n2){
-        if(n1<n3)
+int Min(int n1, int n2, int n3)
+{
+    if (n1 < n2)
+    {
+        if (n1 < n3)
             return n1;
         else
             return n3;
@@ -31,7 +35,7 @@ int main()
 {
     int a, b, c;
     cin >> a >> b >> c;
-    cout << Max(a, b, c)<<endl;
+    cout << Max(a, b, c) << endl;
     cout << Min(a, b, c);
 
     return 0;
