87b42123e431f7aed2a0ee23d1a34d12c41d09cf
Yogesh Singh <yogeshsinghbhati03@gmail.com>
2021-06-20 00:17:51 +0530

    Daily Practice

--- rough.cpp
+++ rough.cpp
@@ -4,33 +4,7 @@ using namespace std;
 
 int main()
 {
-
-    int m ;
-    // cout << "Enter value of m : " << endl;
-    cin >> m;
-    int n = m * 2 + 1;          
-    int arr[n];
-
-    // cout << "Enter input of array"<<endl;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-
-    sort(arr, arr + n);
-
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i] == arr[i+1])
-        {
-            i++;
-        }
-        else{
-            cout << arr[i];
-        }
-        
-    }
-    
+    cout<<(1<<8);             // 0101  0010
 
     return 0;
 }
