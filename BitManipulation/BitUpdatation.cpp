4865c2d5b583e53fbc50c981b95246fc212c8177
Yogesh Singh <yogeshsinghbhati03@gmail.com>
2021-06-20 16:37:31 +0530

    Daily Practice

--- BitManipulation/BitUpdatation.cpp
+++ BitManipulation/BitUpdatation.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int n, pos,value;
+    int n, pos,value;               
     cin >> n >> pos>>value;         
 
     int clearBit = n & (~(1 << pos));       
