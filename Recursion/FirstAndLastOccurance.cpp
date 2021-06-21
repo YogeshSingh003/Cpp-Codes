f9716e54de7f75419d4aeacd87461be66833bc94
Yogesh Singh <yogeshsinghbhati03@gmail.com>
2021-06-22 00:11:22 +0530

    Daily Practice

---
+++ Recursion/FirstAndLastOccurance.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int FirstOcc(int arr[], int n, int i, int key){
+
+    if(i == n)
+        return -1;
+
+    if(arr[i] == key)
+        return i;
+    return FirstOcc(arr,  n, i + 1, key);
+}
+
+int LastOcc(int arr[],int n,int i,int key){
+
+    if(i == n)
+        return -1;
+
+    int restArray = LastOcc(arr, n, i + 1, key);
+    if(restArray != -1)
+        return restArray;
+    
+    if(arr[i] == key)
+        return i;
+
+    return -1;
+}
+int main()
+{
+    int arr[] = {2};
+    cout << FirstOcc(arr, 1, 0, 2)<<endl;
+    cout << LastOcc(arr, 1, 0, 2);
+
+    return 0;
+}
