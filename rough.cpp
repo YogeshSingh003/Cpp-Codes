f0d686b254224c8dc2df915757e5e49dcc31f4ce
Yogesh Singh <yogeshsinghbhati03@gmail.com>
2021-06-26 23:23:42 +0530

    Daily Practice

--- rough.cpp
+++ rough.cpp
@@ -1,54 +1,19 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
+
 using namespace std;
  
 int main()
 {
-    
-    int arr1[4] = {4, 78, 5, 34};
-    int arr2[5] = {46, 8, 54, 3,90};
+    string s = "lala";
+    string a = s;
 
-    int len = 4 + 5;
-
-    int new1[len];
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < a.length()/2; i++)
     {
-        new1[i] = arr1[i];
-    }
-
-    for (int i = 0; i < 5; i++)
-    {
-        new1[i+4] = arr2[i];
-    }
-
-    sort(new1, new1 + len);
-    for (int i = 0; i < len; i++)
-    {
-        cout << new1[i] << " ";
-    }
-    cout << endl;
-
-    for (int i = 0; i < 4; i++)
-    {
-        arr1[i] = new1[i];
-    }
-
-    for (int i = 0; i < 5; i++)
-    {
-        arr2[i] = new1[i+4];
-    }
-    for (int i = 0; i < 4; i++)
-    {
-        cout<<arr1[i]<<" ";
-    }
-    cout << endl;
-    for (int i = 0; i < 5; i++)
-    {
-        cout<<arr2[i]<<" ";
+        char temp = a[i];
+        a[i] = a[a.length() - i - 1];
+        a[a.length() - i - 1] = temp;
     }
 
     
-    
-
     return 0;
 }
