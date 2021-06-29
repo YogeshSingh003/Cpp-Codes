2b7fd9f0d6af08e4ff754f53889ecd7a4b6919cd
Yogesh Singh <yogeshsinghbhati03@gmail.com>
2021-06-29 13:35:01 +0530

    Daily Practice

--- BitManipulation/GetBit.cpp
+++ BitManipulation/GetBit.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 using namespace std;
- 
+
+int getBit(int n, int pos)
+{
+    return (n & (1 << pos)) != 0;
+}
+
 int main()
 {
 
     int n, pos;
     cin >> n >> pos;
 
-    int x = n & (1 << pos);       
-    if(x == (1<<pos))
+    int x = n & (1 << pos);
+    if (x == (1 << pos))
         cout << "1" << endl;
     else
         cout << "0";
