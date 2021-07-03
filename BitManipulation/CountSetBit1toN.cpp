63cd7aa4e88b179176db839c56c0ff92cafcd1b5
Yogesh Singh <yogeshsinghbhati03@gmail.com>
2021-07-03 12:14:22 +0530

    Daily Practice

---
+++ BitManipulation/CountSetBit1toN.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int countSetBits(int n)
+{
+    int totalCount = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        int count = 0;
+        int j = i;
+        while (j > 0)
+        {
+            j = j & (j - 1);
+            count++;
+        }
+        totalCount = totalCount + count;
+    }
+    return totalCount;
+}
+
+int main()
+{
+    cout << countSetBits(4);
+
+    return 0;
+}
+z
