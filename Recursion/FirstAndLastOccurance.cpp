b518453ec283534baec5388ff8d685f8b0e21fa7
Yogesh Singh <yogeshsinghbhati03@gmail.com>
2021-07-07 09:00:39 +0530

    Daily Practice

--- Recursion/FirstAndLastOccurance.cpp
+++ Recursion/FirstAndLastOccurance.cpp
@@ -1,34 +1,42 @@
 #include <iostream>
 using namespace std;
 
-int FirstOcc(int arr[], int n, int i, int key){
+int FirstOcc(int arr[], int n, int i, int key)
+{
 
-    if(i == n)
+    if (i == n)
         return -1;
 
-    if(arr[i] == key)
+    if (arr[i] == key)
         return i;
-    return FirstOcc(arr,  n, i + 1, key);
+    return FirstOcc(arr, n, i + 1, key);
 }
 
-int LastOcc(int arr[],int n,int i,int key){
+int LastOcc(int arr[], int n, int i, int key)
+{
 
-    if(i == n)
+    // if(i == n)
+    //     return -1;
+
+    // int restArray = LastOcc(arr, n, i + 1, key);
+    // if(restArray != -1)
+    //     return restArray;
+
+    // if(arr[i] == key)
+    //     return i;
+
+    // return -1;
+
+    if (i == -1)
         return -1;
-
-    int restArray = LastOcc(arr, n, i + 1, key);
-    if(restArray != -1)
-        return restArray;
-    
-    if(arr[i] == key)
+    if (arr[i] == key)
         return i;
-
-    return -1;
+    return LastOcc(arr, n, i - 1, key);
 }
 int main()
 {
     int arr[] = {2};
-    cout << FirstOcc(arr, 1, 0, 2)<<endl;
+    cout << FirstOcc(arr, 1, 0, 2) << endl;
     cout << LastOcc(arr, 1, 0, 2);
 
     return 0;
--- rough.cpp
+++ rough.cpp
@@ -1,16 +1,41 @@
 #include <iostream>
 using namespace std;
 
-int power(int n, int pow)
+int firstOcc(int arr[], int n, int i, int key)
 {
-    if (pow == 0)
-        return 1;
-    return n * power(n, pow - 1);
+    if (i == n)
+        return -1;
+    if (arr[i] == key)
+        return i;
+    return firstOcc(arr, n, i + 1, key);
 }
 
+int lastOcc(int arr[], int n, int i, int key)
+{
+    // i = n - 1;
+    if (i == -1)
+        return -1;
+    if (arr[i] == key)
+        return i;
+    return lastOcc(arr, n, i - 1, key);
+}
 int main()
 {
+    int n;
+    cout << "Enter the length of array" << endl;
+    cin >> n;
 
-    cout << power(5, 5);
+    int arr[n];
+
+    cout << "Enter input of array" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int key;
+    cin >> key;
+
+    cout << firstOcc(arr, n, 0, key);
+    cout << lastOcc(arr, n, n, key);
     return 0;
 }
