6a22aa68ee2cc847b182ec0b6b7602cd29ac7f1b
Yogesh Singh <yogeshsinghbhati03@gmail.com>
2021-08-02 15:15:05 +0530

    Daily Practice

---
+++ Recursion/ReverseArray.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void rever(int arr[], int i, int n)
+{
+    if (i >= n / 2)
+        return;
+
+    swap(arr[i], arr[n - i - 1]);
+    rever(arr, i + 1, n);
+}
+int main()
+{
+    int n = 5;
+    int arr[5] = {1, 2, 3, 4, 5};
+
+    for (auto e : arr)
+    {
+        cout << e << " ";
+    }
+    cout << endl;
+    rever(arr, 0, n);
+    for (auto e : arr)
+    {
+        cout << e << " ";
+    }
+    return 0;
+}
