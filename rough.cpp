7aa2ca9ea157c5adede8366750410237853e1762
Yogesh Singh <yogeshsinghbhati03@gmail.com>
2021-08-03 22:52:20 +0530

    Daily Practice

--- rough.cpp
+++ rough.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 using namespace std;
 
-void TowerOfHanoi(int n, char src, char dest, char helper)
+void subseq(string s, string ans = "")
 {
-    if (n == 0)
+    if (s.size() == 0)
+    {
+        cout << ans << endl;
         return;
+    }
+    char c = s[0];
+    string ros = s.substr(1);
 
-    TowerOfHanoi(n - 1, src, helper, dest);
-    cout << src << " to " << dest << endl;
-    TowerOfHanoi(n - 1, helper, dest, src);
+    subseq(ros, ans + c);
+    subseq(ros, ans);
 }
 
 int main()
 {
 
-    TowerOfHanoi(3, 'A', 'B', 'C');
+    subseq("ABC", "");
     return 0;
 }
