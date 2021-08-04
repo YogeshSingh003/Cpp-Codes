682e31f771db31277b3b5c76b95a1fb78e72acdc
Yogesh Singh <yogeshsinghbhati03@gmail.com>
2021-08-04 23:41:01 +0530

    Daily Practice

--- Recursion/Permutation.cpp
+++ Recursion/Permutation.cpp
@@ -18,6 +18,29 @@ void per(string s, string ans = "")
     }
 }
 
+void per(string s, int idx)
+{
+    if (s.size() <= idx)
+    {
+        cout << s << endl;
+        return;
+    }
+
+    for (int i = idx; i < s.size(); i++)
+    {
+        swap(s[idx], s[i]);
+        per(s, idx + 1);
+        swap(s[idx], s[i]);
+    }
+}
+
+int main()
+{
+    per("ABC", 0);
+
+    return 0;
+}
+
 int main()
 {
 
