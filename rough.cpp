ddc60d20d818f5f03151045e926ef24e8e91e2e0
Yogesh Singh <yogeshsinghbhati03@gmail.com>
2021-08-12 23:13:47 +0530

    daily practice

--- rough.cpp
+++ rough.cpp
@@ -1,20 +1,34 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-
-int jump(int s, int e)
-{
-    if (e < s)
-        return 0;
-
-    if (e == s)
-        return 1;
-
-    return jump(s, e - 6) + jump(s, e - 5) + jump(s, e - 4) + jump(s, e - 3) + jump(s, e - 2) + jump(s, e - 1);
-}
 
 int main()
 {
 
-    cout << jump(0, 3);
+    vector<int> arr = {3, 7, 2, 7, 2, 8, 4, 4, 5, 4, 3, 4};
+
+    unsigned long long int x = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        x = x + arr[i];
+        if (i < arr.size() - 1)
+            x = x * 10;
+    }
+    x = x + 1;
+    string s = to_string(x);
+    int len = s.size();
+    int a = 0;
+    vector<int> ar(len);
+    for (int i = 0; i < len; i++)
+    {
+        a = x % 10;
+        x = x / 10;
+        ar[len - i - 1] = a;
+    }
+    for (auto e : ar)
+    {
+        cout << e << " ";
+    }
+
     return 0;
 }
